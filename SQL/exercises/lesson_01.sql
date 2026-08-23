73747e76623004cc864081a6bbd22c29ed3232cb
PawelChm <128733608+PawelChm-data@users.noreply.github.com>
2026-08-23 19:10:17 +0200

    Create lesson_01.sql
    SQL fundamentals exercises

---
+++ SQL/exercises/lesson_01.sql
@@ -0,0 +1,18 @@
+-- SQL Fundamentals at SQLBolt
+-- Lessons 1
+-- Dataset: Synthetic transaction data
+
+-- Task 1
+SELECT Title FROM Movies;
+
+-- Task 2
+SELECT Director FROM movies;
+
+-- Task 3
+SELECT Title, Director FROM movies;
+
+-- Task 4
+Select Title, Year FROM movies;
+
+-- Task 5
+SELECT * FROM movies;
