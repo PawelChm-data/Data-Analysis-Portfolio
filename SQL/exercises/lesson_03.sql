9e6a0253723d1b6d4a75a2406c074492f37ff608
PawelChm <128733608+PawelChm-data@users.noreply.github.com>
2026-08-24 13:01:14 +0200

    Create lesson_03.sql

---
+++ SQL/exercises/lesson_03.sql
@@ -0,0 +1,15 @@
+-- SQL Fundamentals at SQLBolt
+-- Lesson: 3
+-- Datset: https://sqlbolt.com/lesson/select_queries_with_constraints_pt_2
+
+-- Task 1
+select * from Movies where Title like "%Toy Story%";
+
+-- Task 2
+select * from Movies where Director = "John Lasseter";
+
+-- Task 3
+select * from Movies where Director != "John Lasseter";
+
+-- Task 4
+select * from Movies where Title like "%WALL%";
