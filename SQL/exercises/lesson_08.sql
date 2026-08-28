1e59556f264e3f621b5b517cd9fe87a1cd9b0e79
PawelChm <128733608+PawelChm-data@users.noreply.github.com>
2026-08-28 21:05:13 +0200

    Create lesson_08.sql

---
+++ SQL/exercises/lesson_08.sql
@@ -0,0 +1,13 @@
+-- SQL Fundamentals at SQLBolt
+-- Lesson: 8
+-- Datset: https://sqlbolt.com/lesson/select_queries_with_nulls
+
+-- Task 1
+select Role, Name, Building from Employees
+where Building is null;
+
+-- Task 2
+Select distinct Building_name from Buildings
+left join Employees
+on Buildings.Building_name = Employees.Building
+where Employees.Role is null;
