d94e8bbb78ce3db076eaf69676ec85d4e00d32a4
PawelChm <128733608+PawelChm-data@users.noreply.github.com>
2026-09-01 21:32:48 +0200

    Create lesson_13.sql

---
+++ SQL/exercises/lesson_13.sql
@@ -0,0 +1,11 @@
+-- SQL Fundamentals at SQLBolt
+-- Lesson: 13
+-- Dataset: https://sqlbolt.com/lesson/inserting_rows
+
+-- Task 1
+insert into Movies
+values (4, 'Toy Story 4', 'Josh Cooley', 2019, 100);
+
+-- Task 2
+insert into Boxoffice
+values (4, 8.7, 340000000, 270000000);
