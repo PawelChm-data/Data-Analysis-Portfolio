d17c3b48023d683ee91292d7912e6dacc71658b1
PawelChm <128733608+PawelChm-data@users.noreply.github.com>
2026-09-03 21:31:47 +0200

    Create lesson_15.sql

---
+++ SQL/exercises/lesson_15.sql
@@ -0,0 +1,11 @@
+-- SQL Fundamentals at SQLBolt
+-- Lesson: 15
+-- Dataset: https://sqlbolt.com/lesson/deleting_rows
+
+-- Task 1
+delete from movies
+where Year < 2005;
+
+-- Task 2
+delete from movies
+where Director = 'Andrew Stanton';
