a5d60fd86d5b0c5d31e6e3585ff73923d4394639
PawelChm <128733608+PawelChm-data@users.noreply.github.com>
2026-09-04 22:10:37 +0200

    Create lesson_16.sql

---
+++ SQL/exercises/lesson_16.sql
@@ -0,0 +1,10 @@
+-- SQL Fundamentals at SQLBolt
+-- Lesson: 16
+-- Dataset: https://sqlbolt.com/lesson/creating_tables
+
+-- Task 1
+CREATE TABLE Database 
+(Name TEXT,
+Version FLOAT,
+Download_count INTEGER);
+
