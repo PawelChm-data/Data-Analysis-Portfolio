ed893d70c49e7c32dc093411560af617b13d2e6f
PawelChm <128733608+PawelChm-data@users.noreply.github.com>
2026-09-06 18:48:50 +0200

    Create lesson_19.sql

---
+++ SQL/exercises/lesson_19.sql
@@ -0,0 +1,9 @@
+-- SQL Fundamentals at SQLBolt
+-- Lesson: 18
+-- Dataset: https://sqlbolt.com/lesson/dropping_tables
+
+-- Task 1
+drop table if exists Movies;
+
+-- Task 2
+drop table if exists Boxoffice;
